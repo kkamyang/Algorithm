3f4b2c86f4d07526f24e0a091abb2eea3e9c9c3d
0207kbk@yonsei.ac.kr <0207kbk@yonsei.ac.kr>
2022-04-21 23:59:04 +0900

    add file

---
+++ BeakJoon/Bronze/bj_b4_9498.py
@@ -0,0 +1,15 @@
+# 9498
+
+def grade(score):
+    if score >= 90:
+        print("A") 
+    elif score >= 80:
+        print("B")
+    elif score >= 70:
+        print("C")
+    elif score >= 60:
+        print("D")
+    else:
+        print("F")
+
+grade(int(input()))
